5f3ada358c24cb53393b7bf8d2dd38b6aaf7af66
EROIIXJIUNDER <asmitadk31@gmail.com>
2024-11-23 16:02:49 +0545

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -1,6 +1,22 @@
 const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
+const API_KEYS = [
+  'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
+  '719775e815msh65471c929a0203bp10fe44jsndcb70c04bc42',
+  'a2743acb5amsh6ac9c5c61aada87p156ebcjsnd25f1ef87037',
+  '8e938a48bdmshcf5ccdacbd62b60p1bffa7jsn23b2515c852d',
+  'f9649271b8mshae610e65f24780cp1fff43jsn808620779631',
+  '8e906ff706msh33ffb3d489a561ap108b70jsne55d8d497698',
+  '4bd76967f9msh2ba46c8cf871b4ep1eab38jsn19c9067a90bb',
+];
+
+// Function to get a random API key from the list
+const getRandomApiKey = () => {
+  const randomIndex = Math.floor(Math.random() * API_KEYS.length);
+  return API_KEYS[randomIndex];
+};
+
 module.exports = {
   name: 'video',
   description: 'Search and play videos.',
@@ -25,7 +41,7 @@ module.exports = {
       }
 
       // Step 2: Get the download link (direct video URL)
-      const downloadResponse = await axios.get(`https://yt-kshitiz.vercel.app/download?id=${videoId}`);
+      const downloadResponse = await axios.get(`https://yt-kshitiz.vercel.app/download?id=${videoId}&apikey=${getRandomApiKey()}`);
       const videoUrl = downloadResponse.data[0];
 
       if (!videoUrl) {
@@ -34,9 +50,6 @@ module.exports = {
       }
 
       // Check if video URL is downloadable and not too large
-      // Assuming videoUrl is a direct file URL that can be streamed
-      // Messenger accepts video attachment URLs, and typically video files should be under 25MB
-
       const videoStreamResponse = await axios.head(videoUrl); // Make a HEAD request to check file size
       const fileSize = parseInt(videoStreamResponse.headers['content-length'], 10);
 
